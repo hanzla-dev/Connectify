992ca4da04951c73c2a1bdeb6a6c64b2eb8397be
hanzla-dev <hanzlaqamar808@gmail.com>
2024-05-26 23:58:45 +0500

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongodb from "./db/connectToMongodb.js";
+const app = express();
 
 dotenv.config();
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
+
 app.get("/" , (req, res) => {
     res.send("hello world!!");
 });
-
-//routes
-app.use("/api/auth", authRoutes);
-
-
 
 
 app.listen(PORT, () => {
