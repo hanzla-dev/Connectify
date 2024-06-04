47ccc789e113fd21556b195ac0ae7b43c1b2d8a3
eshaema <132393790+eshaema@users.noreply.github.com>
2024-06-04 19:31:40 +0500

    Esha

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,20 @@
+import { Route,Routes } from 'react-router-dom';
 import './App.css'
-
-
-import Home from './pages/home/Home.jsx';
-// import Login from './pages/login/Login.jsx';
-// import SignUp from './pages/signup/SignUp.jsx';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import SignUp from './pages/signup/SignUp';
+import {Toaster} from "react-hot-toast"
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Home />
-      {/* <Login /> */}
-      {/* <Signup /> */}
+
+     <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/signup' element={<SignUp />}/>
+     </Routes>
+     <Toaster/>
     </div>
   )
 }
