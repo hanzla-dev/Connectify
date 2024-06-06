fba0e77b57a6cbbb3258df8591c5a69bf92d028e
eshaema <132393790+eshaema@users.noreply.github.com>
2024-06-06 19:04:19 +0500

    esha

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,27 @@
-import { Navigate, Route,Routes } from 'react-router-dom';
-import './App.css'
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import SignUp from './pages/signup/SignUp';
-import {Toaster} from "react-hot-toast"
-import { useAuthContext } from './context/AuthContext';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
+
 function App() {
+  const { authUser } = useAuthContext();
 
-  const {authUser}=useAuthContext();
   return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-
-     <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/login' element={<Login />}/>
-      <Route path='/signup' element={authUser ? <Navigate to="/" />:<SignUp />}/>
-     </Routes>
-     <Toaster/>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
