877bbb9f7d496701590bcde35acedc4a3f05c6fc
eshaema <132393790+eshaema@users.noreply.github.com>
2024-06-19 16:14:01 +0500

    Esha

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,20 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // You can add other aliases here
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:8000",
+			},
+		},
+	},
 });
